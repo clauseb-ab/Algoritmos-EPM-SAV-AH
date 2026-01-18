933fb068f9eaab2d2f7a3d02cf0181d8618a4057
Clau <clau@seba.com>
2026-01-18 20:30:17 -0300

    Agrega funciones de creacion de las bases de datos

--- data_sets.py
+++ data_sets.py
@@ -16,7 +16,7 @@ def create_signed_graph(n, p, probability_negative_edges):
     
     return G
 
-#Funcion para creal grafo de intervalo unitario positivo
+#Funcion para crear grafo de intervalo unitario positivo
 def generar_grafo_intervalo_unitario(n, epsilon):
     # Generar los intervalos unitarios con un control de densidad epsilon
     centros = []
@@ -68,5 +68,95 @@ def graph_to_signed_adjacency_matrix(G):
     
     return adj_matrix
 
-#Fijar semilla 
-random.seed(123)
+def set_complete():
+    #Fijar semilla 
+    random.seed(123)
+
+    # Parámetros
+    vertex_counts = list(range(10, 231, 20))  # Número de vértices de 10 a 230 en pasos de 20
+    p_values=[1]
+    probability_negative_edges = [0.2,0.5,0.8]  # Porcentajes de aristas negativas
+    total_graphs = 108  # Total de grafos
+    generated_graphs = []  # Lista para almacenar los grafos generados
+
+    c = 0  # Contador para identificar cada matriz de adyacencia
+    matrices = {}  # Diccionario para almacenar las Matrices de adyacencia de cada grafo
+
+    for idx, n in enumerate(vertex_counts):
+        for p in p_values:
+            for neg_prob in probability_negative_edges:
+                for i in range(3):  # Generar 12 grafos para cada combinación
+                    c+=1
+                    G = create_signed_graph(n, p, neg_prob)
+                    generated_graphs.append(G)
+
+                    # Convertir el grafo a matriz de adyacencia con signo
+                    adj_matrix = graph_to_signed_adjacency_matrix(G)
+                    
+                    m=[adj_matrix,neg_prob]
+                    
+                    matrices[f'matriz{c}'] = np.array(m, dtype=object)
+
+    print(c)
+
+def set_random():
+    #Fijar semilla 
+    random.seed(216)
+
+    # Parámetros
+    vertex_counts = list(range(10, 251, 20)) 
+    p_values = [0.2, 0.5, 0.8]  # Porcentajes de aristas
+    probability_negative_edges = [0.2,0.5,0.8]  # Porcentajes de aristas negativas
+    total_graphs = 117  # Total de grafos
+    generated_graphs = []  # Lista para almacenar los grafos generados
+
+    c = 0  # Contador para identificar cada matriz de adyacencia
+    matrices = {}  # Para almacenar las Matrices de adyacencia de cada grafo
+
+    for idx, n in enumerate(vertex_counts):
+        for p in p_values:
+            for neg_prob in probability_negative_edges:
+                    c+=1
+                    G = create_signed_graph(n, p, neg_prob)
+                    generated_graphs.append(G)
+
+                    # Convertir el grafo a matriz de adyacencia con signo
+                    adj_matrix = graph_to_signed_adjacency_matrix(G)
+                    
+                    m=[adj_matrix,neg_prob]
+                    
+                    matrices[f'matriz{c}'] = np.array(m, dtype=object)
+
+    print(c)
+
+def set_ui():
+    #Fijar semilla 
+    random.seed(217)
+
+    # Parámetros
+    vertex_counts = list(range(10, 251, 20))  # Número de vértices de 10 a 250 en pasos de 20
+    epsilon_values = [1/8, 1/2,3/4]  # valores de epsilon
+    probability_negative_edges = [0.2, 0.5, 0.8]  # Porcentajes de aristas negativas
+    total_graphs = 117  # Total de grafos que necesitamos (117)
+    generated_graphs = []  # Lista para almacenar los grafos generados
+
+    matrices={}
+    c=0
+
+    for idx, n in enumerate(vertex_counts):
+        for epsilon in epsilon_values:
+            for neg_prob in probability_negative_edges:
+                c+=1
+                G = generar_grafo_intervalo_unitario(n,epsilon)
+                g = agregar_aristas_negativas(G, neg_prob)
+                generated_graphs.append(g)
+
+                # Convertir el grafo a matriz de adyacencia con signo
+                adj_matrix = graph_to_signed_adjacency_matrix(g)
+
+                m=[adj_matrix,epsilon]
+
+                matrices[f'matriz{c}'] = np.array(m, dtype=object)
+
+    print(c)
+
